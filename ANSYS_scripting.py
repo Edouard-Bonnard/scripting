51572b0a832b310ae642604da215de96886af03c
Edouard-Bonnard <edouard.bonnard@gmail.com>
2020-10-05 16:10:28 +0200

    Excel file wirting working

--- ANSYS_scripting.py
+++ ANSYS_scripting.py
@@ -7,7 +7,7 @@
 import os
 from os import listdir
 from os.path import isfile, join, isdir
-import xlrd
+import xlwt
 
 Material = 'Cast Iron'
 Grade = 'FC200'
@@ -26,8 +26,10 @@ os.chdir(dir) #change directory to /Met1
 
 #get the sub directories list
 sub_directories = [d for d in listdir(dir) if isdir(join(dir, d))] 
+
 print(sub_directories)
 
+#sub directories loops
 for a in range(len(sub_directories)):
 
     SAP_ID = sub_directories[a]
@@ -37,17 +39,56 @@ for a in range(len(sub_directories)):
     file_path = sub_directories[a] + '/' + file_name #path creation
     file = open(file_path, "r", encoding = 'utf8') #file loading
 
-    #newfile = []
-
-    lines = file.readlines()
+    lines = file.readlines() #whole file is loaded in lines
 
     Test_Machine_ID = lines[1][10:-1] #removal of \n
-    Date = lines[2][:-1] #removal of \n
-    Original_file = ' '
-    Test_ID = SAP_ID + '-' + Test_Machine_ID
+    Date = lines[2][6:-1] #removal of \n
+    Original_file = os.path.abspath(file_path) #absolute path
+    Test_ID = SAP_ID + '-' + Test_Machine_ID #concatenation of labels
 
-    #for ligne in file:
-    #line = file.readline()
+    # Writing in Info sheet of Excel File
+
+    #Excel file creation
+    xls_file = xlwt.Workbook()
+    info = xls_file.add_sheet("Info")
+    data = xls_file.add_sheet("Data")
+
+    #Filling of info values
+    info.write(0,0,'Property')
+    info.write(0,1,'Value')
+
+    info.write(1,0,'Material')
+    info.write(1,1,Material)
+
+    info.write(2,0,'Grade')
+    info.write(2,1,Grade)
+
+    info.write(3,0,'Supplier')
+    info.write(3,1,Supplier)
+
+    info.write(4,0,'SAP ID')
+    info.write(4,1,SAP_ID)
+
+    info.write(5,0,'Test')
+    info.write(5,1,Test)
+
+    info.write(6,0,'Test Machine ID')
+    info.write(6,1,Test_Machine_ID)
+
+    info.write(7,0,'Date')
+    info.write(7,1,Date)
+
+    info.write(8,0,'Original File')
+    info.write(8,1,Original_file)
+
+    info.write(9,0,'Test ID')
+    info.write(9,1,Test_ID)
+
+    #Saving file
+    xls_file_name = Test_Machine_ID + '.xls' #name of the file
+    xls_file.save(xls_file_name) #save
+    
+
 
 
 
